32f17008690f3dc4e8a7b675cbb3e51eb2f1aba6
Masih <masihalig@gmail.com>
2021-01-30 23:43:41 +0530

    add print-numbers-while.php

---
+++ ch3/print-numbers-while.php
@@ -0,0 +1,8 @@
+<?php
+//In this exercise, we will simply iterate through a while loop to print numbers 1 through 10 and will apply a condition expression to check the numbers are within a range of 1 to 10 as we will be incrementing the number by 1
+$number = 1;
+while ($number <= 10) {
+    echo $number . " ";
+    $number++;
+}
+?>
