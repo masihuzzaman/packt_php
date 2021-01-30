f31119635d886c4138a2eb7bc7741852085693ec
Masih <masihalig@gmail.com>
2021-01-30 16:31:11 +0530

    practice example from packt|php|control statements

---
+++ ch3/test-sunday.php
@@ -0,0 +1,14 @@
+<?php
+/*
+In the following exercise, you will learn to acquire the day using PHP's built-in **`date()`** function. You will be using an **`if...else`** test case to check whether today is Sunday, and then print **`Get rest`** or **`Get ready and go to the office`**:
+*/
+//understand date() function.
+//Here, we have used a built-in date function with a date format flag, l (lowercase L), which returns a textual representation of the current day of the week
+//echo date("l");
+
+if ("Sunday" === date("l")) {
+    echo "Get rest and enjoy your tea with you wife and kids.";
+} else {
+    echo "Get ready, squeeze your breakfast and run to office.";
+}
+?>
