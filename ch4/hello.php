a2fe2298b9690ac5493dce88d571bbaf09415ea3
Masih <masihalig@gmail.com>
2021-01-31 13:00:32 +0530

    add mb_substr() function to support operation on multibyte

--- ch4/hello.php
+++ ch4/hello.php
@@ -5,4 +5,8 @@ echo '<br>';
 echo substr('Hello World', 2);
 echo '<br>';
 echo substr('Hello World', -5, 2);
+echo '<br>';
+echo substr('ideeën', -3);
+echo '<br>';
+echo mb_substr('ideeën', -3);
 ?>
