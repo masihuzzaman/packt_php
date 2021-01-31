68f153d5109c8eb3e61b53da953521f8b4e8bb1e
Masih <masihalig@gmail.com>
2021-01-31 13:06:13 +0530

    add output as comment for substr() function.

--- ch4/hello.php
+++ ch4/hello.php
@@ -1,12 +1,12 @@
 <?php
-echo substr('Hello World', 0, 5);
+echo substr('Hello World', 0, 5); //Hello
 //echo PHP_EOL;
 echo '<br>';
-echo substr('Hello World', 2);
+echo substr('Hello World', 2); //llo World
 echo '<br>';
-echo substr('Hello World', -5, 2);
+echo substr('Hello World', -5, 2); //Wo
 echo '<br>';
-echo substr('ideeën', -3);
+echo substr('ideeën', -3); //ën
 echo '<br>';
-echo mb_substr('ideeën', -3);
+echo mb_substr('ideeën', -3); //eën
 ?>
