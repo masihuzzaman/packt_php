5967b014b327064c07a64bb1fc6d050c1921d1be
Masih <masihalig@gmail.com>
2021-02-27 23:10:58 +0530

    capture query string data

---
+++ activity/movies.php
@@ -0,0 +1,6 @@
+<?php
+//Capture query string data in the file to store the details of the movies, such as the name, the actors, and the release years
+$name = $_GET['movieName'];
+$star = $_GET['movieStar'];
+$year = $_GET['movieYear'];
+?>
